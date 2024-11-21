06a25bc8cb32d2d1e662ec834784788ec84a855b
Brooke Neupert <brookeneupert@Brookes-MacBook-Pro.local>
2024-11-21 15:05:14 -0700

    earth and asteroids animation started

--- NEOWebsite/src/components/Main.jsx
+++ NEOWebsite/src/components/Main.jsx
@@ -2,35 +2,35 @@ export default function Main(props) {
     const {data} = props
     const today = new Date().toISOString().slice(0,10)
     return(
-        <div className="tblContainer">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Designation</th>
-                        <th>Name</th>
-                        <th>Hazardous</th>
-                        <th>Magnitude</th>
-                        <th>Max Diameter (m)</th>
-                        <th>Miss Distance (AU)</th>
-                        <th>Orbits</th>
+        <div className="animation-container">
+            <div class="earth-container">
+                <img src="earth.png" alt="Earth" />
+            </div>
+            <div class = "asteroid-container">
+                {
+                data.near_earth_objects[`${today}`].map((object, index)=> {
+                    return <div class='asteroid-container' id={object.id}>
+                        <img src="asteroid.png" alt={object.name} />
+                        <p>{object.id}</p>
+                    </div>
+                    elt = document.getElementById(object.id)
+
+                    /*
+                    return <tr key = {index}>
+                        <td>{object.id}</td>
+                        <td>{object.name}</td>
+                        <td>{object.is_potentially_hazardous_asteroid.toString()}</td>
+                        <td>{object.absolute_magnitude_h}</td>
+                        <td>{object.estimated_diameter.meters.estimated_diameter_max}</td>
+                        <td>{object.close_approach_data[0].miss_distance.astronomical}</td>
+                        <td>{object.close_approach_data[0].orbiting_body}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    {
-                       data.near_earth_objects[`${today}`].map((object, index) =>{
-                            return <tr key = {index}>
-                                <td>{object.id}</td>
-                                <td>{object.name}</td>
-                                <td>{object.is_potentially_hazardous_asteroid.toString()}</td>
-                                <td>{object.absolute_magnitude_h}</td>
-                                <td>{object.estimated_diameter.meters.estimated_diameter_max}</td>
-                                <td>{object.close_approach_data[0].miss_distance.astronomical}</td>
-                                <td>{object.close_approach_data[0].orbiting_body}</td>
-                            </tr>
-                       })
-                    }
-                </tbody>
-            </table>
+                    */
+                    
+                    })
+                }
+            </div>
+        
         </div>
     )
 }
